e5b746371e7178f6ba5ae55201552d4561429296
paulzay <paulotieno2@gmail.com>
2020-12-24 17:14:51 +0300

    add login and signup to store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import cars from './carReducer';
+import login from './loginReducer';
+import signup from './signupReducer';
 
 const rootReducer = combineReducers({
   cars,
+  login,
+  signup,
 });
 
 export default rootReducer;
