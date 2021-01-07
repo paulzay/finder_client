0acd66115ec9286dcb7d732a3e5300b3ccd538d9
paulzay <paulotieno2@gmail.com>
2021-01-07 13:12:10 +0300

    remove autologin

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,33 @@
 /* eslint-disable react/destructuring-assignment */
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { connect } from 'react-redux';
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Login from './Login/Login';
 import Signup from './Signup/Signup';
 import Cars from './Cars/Cars';
 import Car from './CarView/Car';
 import Nav from '../containers/Nav/Nav';
-import { fetchLoggedInUser } from '../redux/actions/actionCreators';
 import Favorites from '../containers/Favorites/Favorites';
 import PageNotFound from './pageBlank';
 import Home from './Home';
 
-class App extends Component {
-  componentDidMount() {
-    this.loginStatus();
-  }
+export default function App() {
+  return (
+    <div>
 
-  loginStatus = () => {
-    this.props.fetchLoggedInUser();
-  }
-
-  render() {
-    return (
-      <div>
-
-        <BrowserRouter>
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/cars" component={Cars} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-            <Route path="/favorites" component={Favorites} />
-            <Route path="/cars/:id" component={Car} />
-            <Route path="*" component={PageNotFound} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/cars" component={Cars} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/favorites" component={Favorites} />
+          <Route path="/cars/:id" component={Car} />
+          <Route path="*" component={PageNotFound} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
-App.propTypes = {
-  fetchLoggedInUser: PropTypes.func.isRequired,
-};
-const mapDispatchToProps = dispatch => ({
-  fetchLoggedInUser: () => dispatch(fetchLoggedInUser()),
-});
-
-export default connect(null, mapDispatchToProps)(App);
