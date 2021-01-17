c0c784ac8c39b91d1e4a60d63e73bd9012970acb
paulzay <paulotieno2@gmail.com>
2021-01-17 22:05:39 +0300

    modify login and add spinner

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,92 +1,106 @@
-/* eslint-disable react/prop-types, react/destructuring-assignment */
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+/* eslint-disable */
+
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { loginUserFetch } from '../../redux/actions/actionCreators';
+import Spinner from '../Spinner';
+import { login } from '../../redux/actions/index';
+import { SET_MESSAGE } from '../../redux/actions/types';
 import './login.scss';
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      email: '',
-      password: '',
-    };
-  }
+function Login(props) {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  handleSubmit = event => {
-    event.preventDefault();
-    const { loginUserFetch: userLogin } = this.props;
+  const dispatch = useDispatch();
+  const { message } = useSelector(state => state.message);
 
-    const { email, password, username } = this.state;
-    userLogin(
-      {
-        user: {
-          username,
-          email,
-          password,
-        },
-      },
-
-    );
+  const handleChange = e => {
+    if (e.target.name === 'username') {
+      setUsername(e.target.value);
+    } else if (e.target.name === 'email') {
+      setEmail(e.target.value);
+    } else if (e.target.name === 'password') {
+      setPassword(e.target.value);
+    }
   };
 
-  handleChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value,
-    });
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    dispatch(login(username, email, password))
+      .then(response => {
+        if ('error' in response) {
+          dispatch({
+            type: SET_MESSAGE,
+            payload: response.error,
+          });
+          setLoading(false);
+        } else {
+          dispatch({
+            type: SET_MESSAGE,
+            payload: '',
+          });
+          props.history.push('/cars');
+        }
+      })
+      .catch(() => {
+        setLoading(false);
+      });
   };
+  if(loading){
+    return (<Spinner />)
+  }else{
+  return (
+    <div className="login">
+      <h1>Sign in</h1>
+      <p>Hello there! Sign in and start managing your system</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="username"
+          type="text"
+          name="username"
+          value={username}
+          onChange={handleChange}
+        />
+        <input
+          placeholder="email"
+          type="text"
+          name="email"
+          value={email}
+          onChange={handleChange}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        />
+        <button type="submit">
+          Sign In
+        </button>
+        <div className="sgnup">
+          or
+          {' '}
+          <Link to="/signup">Sign up</Link>
+        </div>
 
-  render() {
-    const { username, email, password } = this.state;
-    return (
-      <div className="login">
-        <h1>Sign in</h1>
-        <p>Hello there! Sign in and start managing your system</p>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            placeholder="username"
-            type="text"
-            name="username"
-            value={username}
-            onChange={this.handleChange}
-          />
-          <input
-            placeholder="email"
-            type="text"
-            name="email"
-            value={email}
-            onChange={this.handleChange}
-          />
-          <input
-            placeholder="password"
-            type="password"
-            name="password"
-            value={password}
-            onChange={this.handleChange}
-          />
-          <button type="submit">
-            Sign In
-          </button>
-          <div className="sgnup">
-            or
-            {' '}
-            <Link to="/signup">Sign up</Link>
-          </div>
-
-        </form>
-      </div>
-    );
+      </form>
+    </div>
+  );
   }
 }
-Login.propTypes = {
-  loginUserFetch: PropTypes.func.isRequired,
-};
-const mapDispatchToProps = dispatch => ({
-  loginUserFetch: userInfo => dispatch(loginUserFetch(userInfo)),
-});
 
-export default connect(null, mapDispatchToProps)(Login);
+Login.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+  location: PropTypes.shape({}).isRequired,
+  match: PropTypes.shape({}).isRequired,
+};
+
+export default Login;
